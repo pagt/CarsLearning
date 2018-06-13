e3765cd2e73ab3b9e2bb1758579629405b74ba20
Pierre Angot <pierre.angot@verteego.com>
2018-06-13 17:23:02 +0200

    init cars gameplay

--- game.js
+++ game.js
@@ -50,82 +50,58 @@ var loadImages = function(sources, callback){
 	}
 }
 
-var Bird = function(json){
-	this.x = 80;
-	this.y = 250;
-	this.width = 40;
-	this.height = 30;
+var Car = function(json){
+	this.x = 0;
+	this.y = 0;
+	this.width = 30;
+	this.height = 40;
 
 	this.alive = true;
-	this.gravity = 0;
-	this.velocity = 0.3;
-	this.jump = -6;
+	this.direction = 0;
+	this.speed = 0;
+	this.score = 0;
 
 	this.init(json);
 }
 
-Bird.prototype.init = function(json){
+Car.prototype.init = function(json){
 	for(var i in json){
 		this[i] = json[i];
 	}
 }
 
-Bird.prototype.flap = function(){
-	this.gravity = this.jump;
+Car.prototype.accelerate = function(val){
+	this.speed += val;
+	// you get incentive when you move forward
+	if (val > 0) {
+		this.score += 1;
+	}
 }
 
-Bird.prototype.update = function(){
-	this.gravity += this.velocity;
-	this.y += this.gravity;
+Car.prototype.turn = function(val){
+	this.direction += val;
 }
 
-Bird.prototype.isDead = function(height, pipes){
+Car.prototype.update = function(){
+	this.y += this.speed;
+	// fake direction
+	this.x += this.direction;
+}
+
+Car.prototype.isDead = function(height, width){
+	// Die if you go outside the canvas
 	if(this.y >= height || this.y + this.height <= 0){
 		return true;
 	}
-	for(var i in pipes){
-		if(!(
-			this.x > pipes[i].x + pipes[i].width ||
-			this.x + this.width < pipes[i].x || 
-			this.y > pipes[i].y + pipes[i].height ||
-			this.y + this.height < pipes[i].y
-			)){
-			return true;
-	}
-}
-}
-
-var Pipe = function(json){
-	this.x = 0;
-	this.y = 0;
-	this.width = 50;
-	this.height = 40;
-	this.speed = 3;
-
-	this.init(json);
-}
-
-Pipe.prototype.init = function(json){
-	for(var i in json){
-		this[i] = json[i];
-	}
-}
-
-Pipe.prototype.update = function(){
-	this.x -= this.speed;
-}
-
-Pipe.prototype.isOut = function(){
-	if(this.x + this.width < 0){
+	if(this.x >= width || this.x + this.width <= 0){
 		return true;
 	}
 }
 
+
 var Game = function(){
-	this.pipes = [];
-	this.birds = [];
-	this.score = 0;
-	this.canvas = document.querySelector("#flappy");
+	this.cars = [];
+	this.canvas = document.querySelector("#playground");
 	this.ctx = this.canvas.getContext("2d");
 	this.width = this.canvas.width;
 	this.height = this.canvas.height;
@@ -137,54 +113,55 @@ var Game = function(){
 	this.backgroundSpeed = 0.5;
 	this.backgroundx = 0;
 	this.maxScore = 0;
+	this.currentMaxScore = 0;
 }
 
 Game.prototype.start = function(){
 	this.interval = 0;
-	this.score = 0;
-	this.pipes = [];
-	this.birds = [];
-
+	this.cars = [];
+	this.currentMaxScore = 0;
 	this.gen = Neuvol.nextGeneration();
 	for(var i in this.gen){
-		var b = new Bird();
-		this.birds.push(b)
+		var c = new Car();
+		this.cars.push(c)
 	}
 	this.generation++;
-	this.alives = this.birds.length;
+	this.alives = this.cars.length;
 }
 
 Game.prototype.update = function(){
-	this.backgroundx += this.backgroundSpeed;
-	var nextHoll = 0;
-	if(this.birds.length > 0){
-		for(var i = 0; i < this.pipes.length; i+=2){
-			if(this.pipes[i].x + this.pipes[i].width > this.birds[0].x){
-				nextHoll = this.pipes[i].height/this.height;
-				break;
-			}
-		}
-	}
 
-	for(var i in this.birds){
-		if(this.birds[i].alive){
+
+	for(var i in this.cars){
+		if(this.cars[i].alive){
 
 			var inputs = [
-			this.birds[i].y / this.height,
-			nextHoll
+			this.cars[i].y
 			];
 
+			//NN tells whether accelerate, brake
+			// res returns a float number: if >0.5 accelerate, if <0.5 brake
 			var res = this.gen[i].compute(inputs);
-			if(res > 0.5){
-				this.birds[i].flap();
+			if(res[0] > 0.5){
+				this.cars[i].accelerate(1);
+			} else {
+				this.cars[i].accelerate(-1);
+			}
+			if(res[1] > 0.5){
+				this.cars[i].turn(1);
+			} else {
+				this.cars[i].turn(-1);
 			}
 
-			this.birds[i].update();
-			if(this.birds[i].isDead(this.height, this.pipes)){
-				this.birds[i].alive = false;
+			this.cars[i].update();
+			this.currentMaxScore = (this.cars[i].score > this.currentMaxScore) ? this.cars[i].score : this.currentMaxScore;
+
+			if(this.cars[i].isDead(this.height, this.width)){
+				this.cars[i].alive = false;
 				this.alives--;
 				//console.log(this.alives);
-				Neuvol.networkScore(this.gen[i], this.score);
+				Neuvol.networkScore(this.gen[i], this.cars[i].score);
+				this.maxScore = (this.cars[i].score > this.maxScore) ? this.cars[i].score : this.maxScore;
 				if(this.isItEnd()){
 					this.start();
 				}
@@ -192,29 +169,6 @@ Game.prototype.update = function(){
 		}
 	}
 
-	for(var i = 0; i < this.pipes.length; i++){
-		this.pipes[i].update();
-		if(this.pipes[i].isOut()){
-			this.pipes.splice(i, 1);
-			i--;
-		}
-	}
-
-	if(this.interval == 0){
-		var deltaBord = 50;
-		var pipeHoll = 120;
-		var hollPosition = Math.round(Math.random() * (this.height - deltaBord * 2 - pipeHoll)) +  deltaBord;
-		this.pipes.push(new Pipe({x:this.width, y:0, height:hollPosition}));
-		this.pipes.push(new Pipe({x:this.width, y:hollPosition+pipeHoll, height:this.height}));
-	}
-
-	this.interval++;
-	if(this.interval == this.spawnInterval){
-		this.interval = 0;
-	}
-
-	this.score++;
-	this.maxScore = (this.score > this.maxScore) ? this.score : this.maxScore;
 	var self = this;
 
 	if(FPS == 0){
@@ -230,8 +184,8 @@ Game.prototype.update = function(){
 
 
 Game.prototype.isItEnd = function(){
-	for(var i in this.birds){
-		if(this.birds[i].alive){
+	for(var i in this.cars){
+		if(this.cars[i].alive){
 			return false;
 		}
 	}
@@ -244,29 +198,21 @@ Game.prototype.display = function(){
 		this.ctx.drawImage(images.background, i * images.background.width - Math.floor(this.backgroundx%images.background.width), 0)
 	}
 
-	for(var i in this.pipes){
-		if(i%2 == 0){
-			this.ctx.drawImage(images.pipetop, this.pipes[i].x, this.pipes[i].y + this.pipes[i].height - images.pipetop.height, this.pipes[i].width, images.pipetop.height);
-		}else{
-			this.ctx.drawImage(images.pipebottom, this.pipes[i].x, this.pipes[i].y, this.pipes[i].width, images.pipetop.height);
-		}
-	}
 
 	this.ctx.fillStyle = "#FFC600";
 	this.ctx.strokeStyle = "#CE9E00";
-	for(var i in this.birds){
-		if(this.birds[i].alive){
-			this.ctx.save(); 
-			this.ctx.translate(this.birds[i].x + this.birds[i].width/2, this.birds[i].y + this.birds[i].height/2);
-			this.ctx.rotate(Math.PI/2 * this.birds[i].gravity/20);
-			this.ctx.drawImage(images.bird, -this.birds[i].width/2, -this.birds[i].height/2, this.birds[i].width, this.birds[i].height);
+	for(var i in this.cars){
+		if(this.cars[i].alive){
+			this.ctx.save();
+			this.ctx.translate(this.cars[i].x + this.cars[i].width/2, this.cars[i].y + this.cars[i].height/2);
+			this.ctx.drawImage(images.bird, -this.cars[i].width/2, -this.cars[i].height/2, this.cars[i].width, this.cars[i].height);
 			this.ctx.restore();
 		}
 	}
 
 	this.ctx.fillStyle = "white";
 	this.ctx.font="20px Oswald, sans-serif";
-	this.ctx.fillText("Score : "+ this.score, 10, 25);
+	this.ctx.fillText("Score : "+ this.currentMaxScore, 10, 25);
 	this.ctx.fillText("Max Score : "+this.maxScore, 10, 50);
 	this.ctx.fillText("Generation : "+this.generation, 10, 75);
 	this.ctx.fillText("Alive : "+this.alives+" / "+Neuvol.options.population, 10, 100);
@@ -287,8 +233,8 @@ window.onload = function(){
 
 	var start = function(){
 		Neuvol = new Neuroevolution({
-			population:50,
-			network:[2, [2], 1],
+			population:10,
+			network:[2, [2], 2],
 		});
 		game = new Game();
 		game.start();
